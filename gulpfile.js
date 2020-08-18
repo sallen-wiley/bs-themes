921d45473838bec48f40208357679c7751c300e1
Stuart Allen <sallen@wiley.com>
2020-08-18 14:02:46 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-// var theme = "wiley-online-library";
+var theme = "wiley-online-library";
 // var theme = "wiley-analytical-science";
 // var theme = "wiley-journals-app";
 // var theme = "hub--analytical-science";
-var theme = "hub--nas-open";
+// var theme = "hub--nas-open";
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
